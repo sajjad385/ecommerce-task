2c01907af7709d56eb0c8e2352a1e286f8c10ef4
Mohammad Sajjad Hossain <32493764+sajjad385@users.noreply.github.com>
2023-03-09 11:47:16 +0600

    Update navbar.php

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
   <div class="container">      
-      <a class="navbar-brand" href="http://localhost/ecommerce">PHP Task - BS23</a>
+      <a class="navbar-brand" href="http://localhost/ecommerce-task">
+        
+      </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,10 +10,10 @@
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a class="nav-link" href="http://localhost/ecommerce">All Category</a>
+              <a class="nav-link" href="http://localhost/ecommerce-task">All Category</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="http://localhost/ecommerce/categories/tree">Category Tree</a>
+              <a class="nav-link" href="http://localhost/ecommerce-task/categories/tree">Category Tree</a>
             </li>
           </ul>
         </div>

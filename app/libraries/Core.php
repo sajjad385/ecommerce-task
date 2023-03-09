a07064d16c16e89bad2bb8b532f5316ca055b38a
Mohammad Sajjad Hossain <32493764+sajjad385@users.noreply.github.com>
2023-03-09 11:20:15 +0600

    Update Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,4 +1,5 @@
 <?php
+namespace Sajjad\Ecommerce\Libraries;
   /**
    * App Core Class
    * Creates URL & loads core controller
@@ -27,23 +28,23 @@
       $this->currentController = new $this->currentController;
 
       // Check for second part of url
-      if(isset($url[1])){
+      if(isset($url[1]) && count($url) > 1){
         // Check to see if method exists in controller
-        if(method_exists($this->currentController, $url[1])){
-          $this->currentMethod = $url[1];
-          // Unset 1 index
-          unset($url[1]);
-        }
+          if(method_exists($this->currentController,  end($url))){
+            $this->currentMethod =  end($url);
+            // Unset 1 index
+            array_pop($url);
+          }
       }
 
       // Get params
       $this->params = $url ? array_values($url) : [];
-
       // Call a callback with array of params
       call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl(){
+     
       if(isset($_GET['url'])){
         $url = rtrim($_GET['url'], '/');
         $url = filter_var($url, FILTER_SANITIZE_URL);

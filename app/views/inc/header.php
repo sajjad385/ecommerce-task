b30b2abb546508c190e2e1d955972abfff93140b
Mohammad Sajjad Hossain <32493764+sajjad385@users.noreply.github.com>
2023-03-09 13:12:24 +0600

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,4 +1,4 @@
-
+<?php $root = $_SERVER['DOCUMENT_ROOT'];?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,9 +7,9 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="<?php  $_SERVER['DOCUMENT_ROOT']; ?>/public/css/bootstrap.css">
-  <link rel="stylesheet" href="<?php  $_SERVER['DOCUMENT_ROOT']; ?>/public/css/fontawesome.min.css">
-  <title><?php echo ''; ?></title>
+  <link rel="stylesheet" href="<?php  $root; ?>/public/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php  $root; ?>/public/css/fontawesome.min.css">
+  <title>PHP Task - BS23</title>
 </head>
 <body>
   <?php require $_SERVER['DOCUMENT_ROOT'] .  '/app/views/inc/navbar.php'; ?>

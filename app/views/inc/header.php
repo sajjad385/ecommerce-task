bf1bd96db8fdc00e3c26702f68f2feca33e6c1e9
Mohammad Sajjad Hossain <32493764+sajjad385@users.noreply.github.com>
2023-03-09 11:26:14 +0600

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,11 +7,11 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
-  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/fontawesome.min.css">
-  <title><?php echo SITENAME; ?></title>
+  <link rel="stylesheet" href="<?php  $_SERVER['DOCUMENT_ROOT']; ?>/public/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php  $_SERVER['DOCUMENT_ROOT']; ?>/public/css/fontawesome.min.css">
+  <title><?php echo ''; ?></title>
 </head>
 <body>
-  <?php require APPROOT . '/views/inc/navbar.php'; ?>
+  <?php require $_SERVER['DOCUMENT_ROOT'] .  '/app/views/inc/navbar.php'; ?>
   <div class="container">
   
